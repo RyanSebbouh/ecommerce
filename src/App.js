a2d6fec7f3541d3f1e856a40784c4ea9c77c5ff5
Ryan <ryansebbouh2@gmail.com>
2026-02-09 11:52:12 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+// J'ai retiré l'import du logo car on ne l'utilise plus
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* J'ai supprimé tout le header et j'ai mis le titre demandé */}
+      <h1>Hello react!</h1>
     </div>
   );
 }
